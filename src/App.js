627fad2fc77a55ff3e4e14b3db31d45c2a9d8ae2
TaikiFnit <taikifnitspg@gmail.com>
2018-01-16 19:42:00 +0900

    auto adjust textarea height

--- src/App.js
+++ src/App.js
@@ -6,28 +6,61 @@ import './App.css'
 const textRef = firebaseDb.ref('editable-site/textAreaValue')
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickView = this.onClickView.bind(this);
+  }
+
+  onClickView(e) {
+    this.refs.mainTextArea.focusMainTextArea()
+  }
+
   render() {
     return (
-      <MainTextAreaw />
+      <div className="container" onClick={this.onClickView}>
+        <FnitArt />
+        <MainTextArea ref='mainTextArea' />
+      </div>
     );
   }
 }
 
-class MainTextAreaw extends React.Component {
+function FnitArt(props) {
+  return <header><pre>
+  {`
+/$$$$$$$$        /$$   /$$                               /$$   /$$              
+| $$_____/       |__/  | $$                              |__/  | $$              
+| $$    /$$$$$$$  /$$ /$$$$$$                    /$$$$$$$ /$$ /$$$$$$    /$$$$$$ 
+| $$$$$| $$__  $$| $$|_  $$_/                   /$$_____/| $$|_  $$_/   /$$__  $$
+| $$__/| $$  \\ $$| $$  | $$                    |  $$$$$$ | $$  | $$    | $$$$$$$$
+| $$   | $$  | $$| $$  | $$ /$$                 \\____  $$| $$  | $$ /$$| $$_____/
+| $$   | $$  | $$| $$  |  $$$$/       /$$       /$$$$$$$/| $$  |  $$$$/|  $$$$$$$
+|__/   |__/  |__/|__/   \\___/        |__/      |_______/ |__/   \\___/   \\_______/`}
+  </pre></header>
+}
+
+class MainTextArea extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {textAreaValue: "", isRendered: false}
+    this.state = {textAreaValue: "", textAreaHeight: 0, isRendered: false}
     this.onChangeText = this.onChangeText.bind(this);
     this.onUpdate = this.onUpdate.bind(this);
   }
 
   componentDidMount() {
     this.textRefListener = textRef.on('value', this.onUpdate);
+    this.focusMainTextArea()
   }
 
   componentWillUnmount() {
     textRef.off(this.textRefListener)
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (this.mainTextArea.scrollHeight != this.mainTextArea.clientHeight) {
+      this.setState({textAreaHeight: this.mainTextArea.scrollHeight})
+    }
   }
 
   onUpdate(snapshot) {
@@ -37,29 +70,18 @@ class MainTextAreaw extends React.Component {
 
   onChangeText(e) {
     if (this.state.isRendered) { 
-      this.setState({textAreaValue: e.target.value});
+      this.setState({textAreaValue: e.target.value, textAreaHeight: e.target.scrollHeight});
       textRef.set( e.target.value)
     }
   }
 
+  focusMainTextArea() {
+    this.mainTextArea.focus();
+  }
+
   render() {
     return (
-      <div>
-        <header>
-        <pre>
-        {`
- /$$$$$$$$        /$$   /$$                               /$$   /$$              
- | $$_____/       |__/  | $$                              |__/  | $$              
- | $$    /$$$$$$$  /$$ /$$$$$$                    /$$$$$$$ /$$ /$$$$$$    /$$$$$$ 
- | $$$$$| $$__  $$| $$|_  $$_/                   /$$_____/| $$|_  $$_/   /$$__  $$
- | $$__/| $$  \\ $$| $$  | $$                    |  $$$$$$ | $$  | $$    | $$$$$$$$
- | $$   | $$  | $$| $$  | $$ /$$                 \\____  $$| $$  | $$ /$$| $$_____/
- | $$   | $$  | $$| $$  |  $$$$/       /$$       /$$$$$$$/| $$  |  $$$$/|  $$$$$$$
- |__/   |__/  |__/|__/   \\___/        |__/      |_______/ |__/   \\___/   \\_______/`}
-        </pre>
-        </header>
-        <textarea value={this.state.textAreaValue} onChange={this.onChangeText} />
-      </div>
+        <textarea value={this.state.textAreaValue} onChange={this.onChangeText} ref={(mainTextArea) => {this.mainTextArea = mainTextArea;}} style={{height: this.state.textAreaHeight}}/>
     );
   }
 }
