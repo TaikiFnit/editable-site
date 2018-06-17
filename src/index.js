16e2c999839b08c83307e82c3b9884ead2a4c422
TaikiFnit <taikifnitspg@gmail.com>
2018-06-17 15:10:27 +0900

    complete introduce again

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import TextSharing from './components/TextSharing';
 import registerServiceWorker from './registerServiceWorker';
+import AppFrame from './components/AppFrame';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from './theme/muiTheme';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <AppFrame>
+      <TextSharing />
+    </AppFrame>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/theme/muiTheme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    textColor: '#ffffff'
+  },
+  typography: {
+    display3: {
+      color: 'rgb(255,255,255)'
+    }
+  }
+});
